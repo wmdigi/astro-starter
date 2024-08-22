0b9177efc785c12dfb3b8c05e6b908f385750b3c
Gabriel Uhlíř <69206870+gauel@users.noreply.github.com>
2024-08-22 12:05:44 +0200

    fix(Global): some style and import errors

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,20 +1,9 @@
 import Layout from "./Layout.astro";
 import Logo from "@components/Logo.astro";
 import ThemeToggle from "@components/ThemeToggle.astro";
-//import MenuToggle from "@components/MenuToggle.svelte";
 import Link from "@components/ui/Link.astro";
 import Button from "@components/ui/Button.astro";
 import Input from "@components/ui/Input.astro";
 import Checkbox from "@components/ui/Checkbox.astro";
 import Marquee from "@components/Marquee.astro";
-export {
-	Layout,
-	Logo,
-	ThemeToggle,
-	//MenuToggle,
-	Link,
-	Button,
-	Input,
-	Checkbox,
-	Marquee
-};
+export { Layout, Logo, ThemeToggle, Link, Button, Input, Checkbox, Marquee };
