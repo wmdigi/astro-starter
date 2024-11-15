84cd5f4dc6518d5894aad8a2b619635bf7ca78d0
Krystof Cernik <96994380+KrystofCernik@users.noreply.github.com>
2024-11-15 11:55:17 +0100

    feat(Global): sorry, too many changes, Auth testing

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
 import { defineMiddleware } from 'astro:middleware';
-import { createDirectus, rest, readMe, authentication } from '@directus/sdk';
+import { createDirectus, rest, readMe, authentication, refresh } from '@directus/sdk';
 
 const client = createDirectus('https://wiktor.wondermakers.dev').with(authentication()).with(rest());
 
@@ -8,14 +8,22 @@ export const onRequest = defineMiddleware(async (context, next) => {
 	console.log("Context[Middleware.ts]: ", context.locals)
 	console.log("Cookies[Middleware.ts]: ", context.cookies);
 
-	const access_token =  context.cookies.get("access_token")?.value
+	const current_access_token =  context.cookies.get("access_token")?.value
+	const current_refresh_token =  context.cookies.get("refresh_token")?.value
 
-	if (access_token) {
+	if (current_access_token) {
 		try {
-			client.setToken(access_token);
-			const user = await client.request(readMe());
-			context.locals.user = user;
-			console.log("Relogged in[Middleware.ts]: ", context.locals.user)
+			const { access_token, refresh_token } = await client.request(refresh("json", current_refresh_token));
+
+			if (access_token && refresh_token) {
+				client.setToken(access_token);
+				context.cookies.set("refresh_token", refresh_token)
+
+				const user = await client.request(readMe());
+				context.locals.user = user;
+				console.log("Relogged in[Middleware.ts]: ", context.locals.user)
+			}
+
 		  } catch (error) {
 			console.error("Auth error:", error);
 		  }
